7ec877ac3301b914d3ef974d1b85733d81daf32a
Farrukh Askari <farrukh.askari01@gmail.com>
2015-09-26 00:09:22 +0500

    Nodejs Todo App without multiple eneteries
    Adding files via Github Desktop App

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name todoAppApp
+ * @description
+ * # todoAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('todoAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ui.sortable',
+    'LocalStorageModule'
+  ])
+  .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+    localStorageServiceProvider.setPrefix('ls');
+  }])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
